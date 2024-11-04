2ed9d4950cd205ad8fd6e12c8778527f4dd3c01b
haliyikama1 <22092024yeni@gmail.com>
2024-11-04 12:29:47 +0300

    1231

--- src/app/okullara-mazot-servisi/page.jsx
+++ src/app/okullara-mazot-servisi/page.jsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function OkullaraMazotServisi() {
     return (
